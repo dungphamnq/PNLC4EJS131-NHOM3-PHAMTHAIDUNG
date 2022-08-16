ae0f24e8f19f023b73f2cd6225822cb750514b92
dungphamnq <dungphamnq@gmail.com>
2022-08-16 14:42:17 +0700

    add

---
+++ hw7/hw7.js
@@ -0,0 +1,19 @@
+const str = 'Hôm nay ngày thứ 7'
+let a = str.charAt(0).toLowerCase(); // "h"
+let b = str.charAt(8).toUpperCase(); // "N"
+let c = str.slice(1, 8); // "ôm nay "
+let d = str.slice(9, 18); // "gày thứ 7"
+
+let str1 = a.concat(c, b, d);
+console.log(str1);
+
+let g = str.slice(1, 3);
+let h = str.slice(4, 17);
+let str2 = a.concat(g, h, "8");
+
+console.log("str2", str2);
+
+
+
+
+
